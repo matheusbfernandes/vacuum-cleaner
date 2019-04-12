04daca6c22dc29036fd1bd04a994ea92e06af75c
Matheus Fernandes <matheusfernandes1981@hotmail.com>
2019-04-12 03:38:31 -0300

    versão final

--- controls.js
+++ controls.js
@@ -1,27 +1,11 @@
-var roomTheRobotIs = 0;
-var dirtiness = [0, 0, 0, 0];
-var DIRTY_AMOUNT = 15;
-var greedMode = true;
-var auto = true;
+let roomTheRobotIs = 0;
+let dirtiness = [0, 0, 0, 0];
+let auto = true;
 
 $(document).ready(function() {
     $('.room').click(function() {
         increaseDirty(this.id);
     });
-
-    function increaseDirty(i){
-        if (dirtiness[i] < 2) {
-            dirtiness[i] += 1;
-            switch (dirtiness[i]) {
-                case 1:
-                    $('#' + i).removeClass('button-no-dirt').addClass('button-dirt');
-                    break;
-                case 2:
-                    $('#' + i).removeClass('button-dirt').addClass('button-dirt-2');
-                    break;
-            }
-        }
-    }
 
     $('#RIGHT').click(function() {
         right();
@@ -53,28 +37,29 @@ $(document).ready(function() {
         }
     });
 
-    $('#MODE').click(function() {
-        if(auto) {
-            if(greedMode) {
-                greedMode = false;
-                $(this).text('Cycle Mode');
-                $("#r1").css("background-color", "#6464b6");
-            } else {
-                greedMode = true;
-                $(this).text('Greed Mode');
-                $("#r1").css("background-color", "#64b664");
-            }
-        }
-    });
-
     automaticMode();
 
 });
 
+function increaseDirty(i){
+    if (dirtiness[i] < 2) {
+        dirtiness[i] += 1;
+        let ind = $('#' + i);
+        switch (dirtiness[i]) {
+            case 1:
+                ind.removeClass('button-no-dirt').addClass('button-dirt');
+                break;
+            case 2:
+                ind.removeClass('button-dirt').addClass('button-dirt-2');
+                break;
+        }
+    }
+}
+
 function cleanRoom(roomId){
     if(dirtiness[roomId] === 2){
         dirtiness[roomTheRobotIs] = 0;
-        $('#action').text("Cleaning...").css({'font-family': 'Arial, serif;'});
+        $('#action').text("Cleaning...");
         setTimeout(
             () => {
                         $('#action').text("");
@@ -85,7 +70,7 @@ function cleanRoom(roomId){
     }
     else if (dirtiness[roomId] === 1) {
         dirtiness[roomTheRobotIs] = 0;
-        $('#action').text("Cleaning...").css({'font-family': 'Arial, serif;'});
+        $('#action').text("Cleaning...");
         setTimeout(
             () => {
                 $('#action').text("");
@@ -100,7 +85,7 @@ function right() {
     if(roomTheRobotIs === 0){
         roomTheRobotIs = 1;
     }
-    if(roomTheRobotIs === 2){
+    else if(roomTheRobotIs === 2){
         roomTheRobotIs = 3;
     }
 
@@ -111,7 +96,7 @@ function left() {
     if(roomTheRobotIs === 1){
         roomTheRobotIs = 0;
     }
-    if(roomTheRobotIs === 3){
+    else if(roomTheRobotIs === 3){
         roomTheRobotIs = 2;
     }
 
@@ -122,7 +107,7 @@ function down() {
     if(roomTheRobotIs === 0){
         roomTheRobotIs = 2;
     }
-    if(roomTheRobotIs === 1){
+    else if(roomTheRobotIs === 1){
         roomTheRobotIs = 3;
     }
 
@@ -133,7 +118,7 @@ function up() {
     if(roomTheRobotIs === 2){
         roomTheRobotIs = 0;
     }
-    if(roomTheRobotIs === 3){
+    else if(roomTheRobotIs === 3){
         roomTheRobotIs = 1;
     }
 
@@ -205,11 +190,12 @@ function decideRoom(){
         cleanRoom(roomTheRobotIs);
         return;
     }
-    if(dirtiness[getNextRoom()] > dirtiness[getPreviousRoom()]){
-        goToNextRoom();
+
+    if(dirtiness[getNextRoom()] < dirtiness[getPreviousRoom()]){
+        goToPreviousRoom();
     }
     else{
-        goToPreviousRoom();
+        goToNextRoom();
     }
 }
 
@@ -230,20 +216,17 @@ function automaticMode(){
             );
         }
         else{
-            if(greedMode) {
-                decideRoom();
-            } else {
-                goToPreviousRoom();
-            }
+            decideRoom();
             setTimeout(
                 () => {automaticMode()},
                 3000
-        );
+            );
         }
-    } else {
+    }
+    else {
         setTimeout(
-            () => { automaticMode() },
+            () => {automaticMode()},
             2000
-    );
+        );
     }
 }
